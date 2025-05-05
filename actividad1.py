f40996fb3b9bc998298ba053b12cbb60437997dd
cinnamonjvt <ja.mansillac@duocuc.cl>
2025-05-05 00:14:39 -0400

    actividad1.py

--- actividad1.py
+++ actividad1.py
@@ -1,12 +1,8 @@
-# Definimos las credenciales correctas
 usuario_correcto = "cinamon"
 contrasena_correcta = "roll2007"
-
-# Solicitamos credenciales al usuario
 usuario_ingresado = input("Ingrese su nombre de usuario: ")
 contrasena_ingresada = input("Ingrese su contraseña: ")
 
-# Verificamos las credenciales
 if usuario_ingresado == usuario_correcto and contrasena_ingresada == contrasena_correcta:
     print(f"¡Bienvenido/a {usuario_ingresado}! Acceso concedido.")
 elif usuario_ingresado == usuario_correcto:
